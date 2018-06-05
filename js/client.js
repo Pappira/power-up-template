dbdc8c910bba9a766343006eb95e33b69ebe77e5
aarchimowicz <aron.archimowicz@moove-it.com>
2018-06-05 11:23:50 -0300

    popup vs modal

--- js/client.js
+++ js/client.js
@@ -328,12 +328,23 @@ TrelloPowerUp.initialize({
     return [{
       text: 'Nuevo Presupuesto',
       callback: function(t){
+        return t.popup({
+          title: 'Nuevo Presupuesto',
+          url: './new-card.html',
+          args: { invalidations: validationBadge.invalidations },
+          height: 240
+        });
+      }
+    },
+    {
+      text: 'Nuevo Presupuesto',
+      callback: function(t){
         return t.modal({
           url: './new-card.html', // The URL to load for the iframe
           args: { text: 'Hello' }, // Optional args to access later with t.arg('text') on './modal.html'
           accentColor: '#F2D600', // Optional color for the modal header 
           height: 500, // Initial height for iframe; not used if fullscreen is true
-          fullscreen: true, // Whether the modal should stretch to take up the whole screen
+          fullscreen: false, // Whether the modal should stretch to take up the whole screen
           callback: () => console.log('Goodbye.'), // optional function called if user closes modal (via `X` or escape)
           title: 'Nuevo Presupuesto', // Optional title for modal header
           // You can add up to 3 action buttons on the modal header - max 1 on the right side.
