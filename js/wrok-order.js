cca864a801f45c2ed816199613acc48e22d7abd0
ylan <ylanar@gmail.com>
2018-06-07 12:51:51 -0300

    Strating with workOrderPDF

---
+++ js/wrok-order.js
@@ -0,0 +1,32 @@
+
+var workOrderPDF = function(){
+
+    var pdf = new jsPDF('p', 'pt', 'letter');
+    source = '<footer><div style="text-align:center;">Page <span class="pageCounter"></span>/<span class="totalPages"></span></div></footer>';
+
+    specialElementHandlers = {
+        '#bypassme': function (element, renderer) {
+            return true
+        }
+    };
+    margins = {
+        top: 80,
+        bottom: 60,
+        left: 40,
+        width: 522
+    };
+
+    pdf.fromHTML(
+        source, 
+        margins.left, // x coord
+        margins.top, { // y coord
+            'width': margins.width, 
+            'elementHandlers': specialElementHandlers
+        },
+
+        function (dispose) {
+            pdf.save('Prueba.pdf');
+        }, margins
+    );
+
+};
