22ecbe1f6bdf6d6c27875218deb0f942343cd74b
ylan <ylanar@gmail.com>
2018-06-09 10:36:40 -0300

    WorkOrder PDF in a modal instead of download

--- js/work-order.js
+++ js/work-order.js
@@ -111,11 +111,8 @@ var workOrderPDF = function(estimate){
     heigth += rowSize;
     doc.text(firstColumn, heigth+rowSize + rowSize-1.5, "<Comentarios>");
     
-    //doc.output('datauri');
+    return doc.output('bloburl');
 
-    //window.open(doc.output('bloburl'),'_parent');
-
-    doc.save('demo.pdf'); 
 };
 
 var writeTextInDoc = function(doc,name,value,x,y,boxLength,boxBackgroundColor,fontColor){
@@ -149,6 +146,15 @@ var getWorkOrderPDFCallBack = function(t){
     var cardInfoKey = 'pappira.cardInfo';
     return t.get('card', 'shared', cardInfoKey)
     .then(function(cardInfo){
-        workOrderPDF(cardInfo);
+        return t.modal({
+            url: workOrderPDF(cardInfo), // The URL to load for the iframe
+            args: {}, // Optional args to access later with t.arg('text') on './modal.html'
+            accentColor: '#F2D600', // Optional color for the modal header 
+            height: 1500, // Initial height for iframe; not used if fullscreen is true
+            fullscreen: false, // Whether the modal should stretch to take up the whole screen
+            title: 'Orden de Trabajo', // Optional title for modal header
+            // You can add up to 3 action buttons on the modal header - max 1 on the right side.
+            actions: [],
+        });
     });
 };
