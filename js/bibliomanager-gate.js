ee538a81de15688d2f3586b6a135e616ef76439c
aarchimowicz <aron.archimowicz@moove-it.com>
2019-01-11 19:32:45 -0300

    Add cardStatus

--- js/bibliomanager-gate.js
+++ js/bibliomanager-gate.js
@@ -2,23 +2,9 @@ var Promise = TrelloPowerUp.Promise;
 var bibliomanagerGateHost = "https://bibliomanager-gate.lad.uy/api";
 
 var getCardStatus = function(cardId) {
-  return new Promise(function(resolve,reject) {
-    $.ajax({
-      type: "GET",
-      url:
-        bibliomanagerGateHost + "/trello/card/5c389be96404ec44a8367438/status",
-      success: function(result) {
-        return resolve(result);
-      },
-      error: function(result) {
-        //handle the error
-        console.log(result);
-        return reject();
-      }
-    });
-    resolve({
-      name: "💻 " + options.url + " 🤔",
-      desc: "This Power-Up knows cool things about the attached url"
-    });
-  });
+  return Promise.resolve(
+    $.get(
+      bibliomanagerGateHost + "/trello/card/5c389be96404ec44a8367438/status"
+    )
+  );
 };
