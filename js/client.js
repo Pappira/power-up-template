63c633e6724a2e52071995ea5feef8e7490cb372
Ylan1987 <ylanar@gmail.com>
2019-01-12 17:08:54 -0300

    strill trying

--- js/client.js
+++ js/client.js
@@ -539,13 +539,13 @@ TrelloPowerUp.initialize({
           icon: GRAY_ICON, // don't use a colored icon here
           text: 'Poner precios',
           callback: getAddPrices()
-        },{
+        },/*{
           // usually you will provide a callback function to be run on button click
           // we recommend that you use a popup on click generally
           icon: GRAY_ICON, // don't use a colored icon here
           text: 'Ver O.T.',
           callback: getWorkOrderPDFCallBack
-        },/*{
+        },{
           // usually you will provide a callback function to be run on button click
           // we recommend that you use a popup on click generally
           icon: GRAY_ICON, // don't use a colored icon here
