d49036d241eb5c7bd2003b7c093d7bd24ecc38fc
Ylan1987 <ylanar@gmail.com>
2019-01-29 11:01:46 -0300

    multi item work added to works json

--- js/work.js
+++ js/work.js
@@ -70,5 +70,236 @@ var works = [
           ]
         }
       ]
+    },
+    {
+      "id":1,
+      "workTypeId":1,
+      "workType":"Encuadernados",
+      "clossedSizes":["150x210mm"],
+      "image":noImage,
+      "name":"Cuaderno",
+      "quantities":[100,200,300,500,1000],
+      "mandatoryFinishGroups":
+      [
+        {
+          "groupName": "Encuadernado",
+          "finishes":
+          [
+            {
+              "finish":"Encuadernado HotMelt",
+              "finishComment":"",
+              "showToClientFinish":true,
+              "incidences":
+              [
+                {
+                  "itemId":-1,
+                  "type":"optionalFinishes",
+                  "action":"add",
+                  "values":
+                  [
+                    {
+                      "finish":"Marca Páginas",
+                      "finishComment":"",
+                      "showToClientFinish":true
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              "finish":"Encuadernado con Rulo Wire-o",
+              "finishComment":"",
+              "showToClientFinish":true
+            }
+          ]
+        }
+      ],
+      
+      "items":
+      [
+        {
+          "id":0,
+          "name":"Tapa y contratapa",
+          "bleedPrint":true,
+          "inks":[
+            {
+              "inksDetails":"Full color",
+              "inksQuantity":4
+            }
+          ],
+          "openedSize":["150x210mm"],
+          "allTheSame":false,
+          "quantityOfPages":[2],
+          "quantityOfVias":[1],
+          "faces":["Simple Faz"],
+          "materials":[
+            {
+              "paper":"Cartulina Blanca/Blanca",
+              "gr":"250"
+            },
+            {
+              "paper":"Coteado",
+              "gr":"300"
+            },
+            {
+              "paper":"Coteado",
+              "gr":"150"
+            }
+          ],
+          "mandatoryFinishGroups":
+          [
+            {
+              "groupName": "Tipo de Tapa",
+              "finishes": 
+              [
+                {
+                  "finish":"Tapa Flexible",
+                  "finishComment":"",
+                  "showToClientFinish":true,
+                  "incidences":
+                  [
+                    {
+                      "itemId":0,
+                      "type":"materials",
+                      "action":"replace",
+                      "values":
+                      [
+                        {
+                          "paper":"Cartulina Blanca/Blanca",
+                          "gr":"250"
+                        },
+                        {
+                          "paper":"Coteado",
+                          "gr":"300"
+                        }
+                      ]
+                    }
+                  ]
+                },
+                {
+                  "finish":"Tapa SemiDura",
+                  "finishComment":"",
+                  "showToClientFinish":true,
+                  "incidences":
+                  [
+                    {
+                      "itemId":0,
+                      "type":"materials",
+                      "action":"replace",
+                      "values":
+                      {
+                        "paper":"Cartulina Blanca/Blanca",
+                        "gr":"250"
+                      }
+                    },
+                    {
+                      "itemId":-1,
+                      "type":"optionalFinishes",
+                      "action":"add",
+                      "values":
+                      [
+                        {
+                          "finish":"Cierre Elástico",
+                          "finishComment":"",
+                          "showToClientFinish":true
+                        }
+                      ]
+                    }
+                  ]
+                },
+                {
+                  "finish":"Tapa Dura",
+                  "finishComment":"",
+                  "showToClientFinish":true,
+                  "incidences":
+                  [
+                    {
+                      "itemId":0,
+                      "type":"materials",
+                      "action":"replace",
+                      "values":
+                      {
+                        "paper":"Coteado",
+                        "gr":"150"
+                      }
+                    },
+                    {
+                      "itemId":-1,
+                      "type":"optionalFinishes",
+                      "action":"add",
+                      "values":
+                      [
+                        {
+                          "finish":"Cierre Elástico",
+                          "finishComment":"",
+                          "showToClientFinish":true
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]  
+            }
+          ],
+          "optionalFinishes":
+          [
+            {
+              "finish":"Laminado Mate",
+              "comment":"",
+              "showToClient":true
+            },
+            {
+              "finish":"Laminado Brillo",
+              "comment":"",
+              "showToClient":true
+            },
+            {
+              "finish":"Laminado Mate o Brillo a definir",
+              "comment":"",
+              "showToClient":true
+            }
+          ]
+        },
+        {
+          "id":1,
+          "name":"Intrior",
+          "bleedPrint":true,
+          "inks":
+          [
+            {
+              "inksDetails":"Sin imprimir",
+              "inksQuantity":0
+            },
+            {
+              "inksDetails":"Tinta Negra",
+              "inksQuantity":1
+            },
+            {
+              "inksDetails":"2 tintas",
+              "inksQuantity":2
+            }
+          ],
+          "openedSize":["150x210mm"],
+          "allTheSame":false,
+          "quantityOfPages":[70,100],
+          "quantityOfVias":[1],
+          "faces":["Doble Faz"],
+          "materials":
+          [
+            {
+              "paper":"Obra Blanco",
+              "gr":"80"
+            },
+            {
+              "paper":"Obra Blanco",
+              "gr":"90"
+            },
+            {
+              "paper":"Rayado estándar",
+              "gr":"70"
+            }
+          ]
+        } 
+      ]
     }
   ];
