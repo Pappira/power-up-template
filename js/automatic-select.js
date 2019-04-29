32c303007bb1d9548d40acb742af2599c7f3ddb4
Ylan1987 <ylanar@gmail.com>
2019-04-29 11:37:54 -0300

    bug fix on creating automatic select panel

--- js/automatic-select.js
+++ js/automatic-select.js
@@ -140,39 +140,37 @@ var createPossibilities = function(work){
   var possibilities = [];
   for(var attr in work){
     if(typeof work[attr] == 'object' && attr != "mandatoryFinishGroups"){
-      if(attr == "optionalFinishes" && work[attr].length >0){
-        if (attr != 'items'){
-          if (work[attr].length > 1){
-            var possibility = {};
-            possibility['itemId'] = -1;
-            possibility['itemName'] = 'general';
-            possibility['name'] = attr;
-            possibility['values'] = work[attr];
-            possibilities.push(possibility);
-          }
-        }else{
-          var items = work.items;
-          for (var i = 0; i < items.length;i++){
-            var item = items[i];
-            for(var itemAttr in item){
-              if(typeof item[itemAttr] == 'object' && itemAttr != "mandatoryFinishGroups"){
-                if ((item[itemAttr].length > 1) || (itemAttr == "optionalFinishes" && work[attr].length >0)){
+      if (attr != 'items'){
+        if((work[attr].length > 1) || (attr == "optionalFinishes" && work[attr].length >0)){
+          var possibility = {};
+          possibility['itemId'] = -1;
+          possibility['itemName'] = 'general';
+          possibility['name'] = attr;
+          possibility['values'] = work[attr];
+          possibilities.push(possibility);
+        }
+      }else{
+        var items = work.items;
+        for (var i = 0; i < items.length;i++){
+          var item = items[i];
+          for(var itemAttr in item){
+            if(typeof item[itemAttr] == 'object' && itemAttr != "mandatoryFinishGroups"){
+              if ((item[itemAttr].length > 1) || (itemAttr == "optionalFinishes" && work[attr].length >0)){
+                var possibility = {};
+                possibility['itemId'] = i;
+                possibility['itemName'] = item.name;
+                possibility['name'] = itemAttr;
+                possibility['values'] = item[itemAttr];
+                possibilities.push(possibility);
+              }
+            }else if(itemAttr == "mandatoryFinishGroups"){
+              for (var j = 0; j < item[itemAttr].length ; j++){
                   var possibility = {};
                   possibility['itemId'] = i;
-                  possibility['itemName'] = item.name;
-                  possibility['name'] = itemAttr;
-                  possibility['values'] = item[itemAttr];
+                  possibility['itemName'] =  item.name;
+                  possibility['name'] = itemAttr + " // " + j;
+                  possibility['values'] = item[itemAttr][j].finishes;
                   possibilities.push(possibility);
-                }
-              }else if(itemAttr == "mandatoryFinishGroups"){
-                for (var j = 0; j < item[itemAttr].length ; j++){
-                    var possibility = {};
-                    possibility['itemId'] = i;
-                    possibility['itemName'] =  item.name;
-                    possibility['name'] = itemAttr + " // " + j;
-                    possibility['values'] = item[itemAttr][j].finishes;
-                    possibilities.push(possibility);
-                }
               }
             }
           }
