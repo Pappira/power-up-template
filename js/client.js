38ed00f1c8d83c4c1a1c1d58cc635e90f3b1d500
Ylan1987 <ylanar@gmail.com>
2019-09-02 13:39:52 -0300

    bug fix on client.js

--- js/client.js
+++ js/client.js
@@ -414,24 +414,30 @@ TrelloPowerUp.initialize({
           acceptEstimte = {
             // usually you will provide a callback function to be run on button click
             // we recommend that you use a popup on click generally
-            icon: GRAY_ICON, // don't use a colored icon here
+            //icon: GRAY_ICON, // don't use a colored icon here
             text: 'Aceptar',
             callback: getAcceptEstimate()
           }
         }
-        return [{
+        return [acceptEstimte,{
           // usually you will provide a callback function to be run on button click
           // we recommend that you use a popup on click generally
-          icon: GRAY_ICON, // don't use a colored icon here
+          //icon: GRAY_ICON, // don't use a colored icon here
           text: 'Cliente',
           callback: getCustomerCallback()
         },{
           // usually you will provide a callback function to be run on button click
           // we recommend that you use a popup on click generally
-          icon: GRAY_ICON, // don't use a colored icon here
+          //icon: GRAY_ICON, // don't use a colored icon here
           text: 'Modificar',
           callback: getNewEstimateModalCallback()
-        }];
+        },{
+          // usually you will provide a callback function to be run on button click
+          // we recommend that you use a popup on click generally
+          //icon: GRAY_ICON, // don't use a colored icon here
+          text: 'Ver Presupuesto',
+          callback: getEstimateCallBack
+        },acceptEstimte];
       }
     );
   },
