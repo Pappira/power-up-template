d9766092d300629c290135bb85694249aa8aa6bd
Ylan1987 <ylanar@gmail.com>
2019-09-02 13:40:39 -0300

    same button was 2 times on card, now only 1

--- js/client.js
+++ js/client.js
@@ -419,7 +419,7 @@ TrelloPowerUp.initialize({
             callback: getAcceptEstimate()
           }
         }
-        return [acceptEstimte,{
+        return [{
           // usually you will provide a callback function to be run on button click
           // we recommend that you use a popup on click generally
           //icon: GRAY_ICON, // don't use a colored icon here
